6406d625208605a6abc7024d214550b9700cce42
israt6898 <israt6898@gmail.com>
2024-04-07 23:22:32 +0600

    start real estate project

---
+++ src/firebase/firebase.confiq.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAX9iU4zpU-_eKCd5YWXJZJLzpqZkYgEjQ",
+  authDomain: "real-estate-project-92be1.firebaseapp.com",
+  projectId: "real-estate-project-92be1",
+  storageBucket: "real-estate-project-92be1.appspot.com",
+  messagingSenderId: "1069019865207",
+  appId: "1:1069019865207:web:c2ad07fad560411369e672"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
